d2884af586a13246530eff44eebee3276e251896
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 20:40:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,8 +34,9 @@ def select_series_title_with_most_human_characters
   FROM series
   LEFT OUTER JOIN characters
   ON series.id = characters.series_id
-  GROUP BY characters.species
   WHERE characters.species = 'human'
+  GROUP BY characters.species
+  
   ORDER BY COUNT(characters.species) DESC
   LIMIT 1;"
 end
