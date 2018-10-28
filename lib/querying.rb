fbd1af13bb0a858383c61f6e0b467cfd36e97ced
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 20:37:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,9 +36,8 @@ def select_series_title_with_most_human_characters
   ON series.id = characters.series_id
   GROUP BY characters.species
   ORDER BY COUNT(characters.species) DESC
-  WHERE characters.species = 'human'
-  LIMIT 1;
- "
+  LIMIT 1
+  WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
