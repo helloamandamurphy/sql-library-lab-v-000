a8607d8791091f5ffc354d0929d3dcd9a452201d
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 20:39:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
-  LEFT OUTER JOIN characters
+  JOIN characters
   ON series.id = characters.series_id
   GROUP BY characters.species
   WHERE characters.species = 'human'
