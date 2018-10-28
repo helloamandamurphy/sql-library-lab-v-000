26fba15f007bd577556223751fd4c9c6b757cf30
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 20:50:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM series
+  FROM authors
   JOIN subgenres 
   ON series.subgenres_id = subgenres.id
   JOIN authors
