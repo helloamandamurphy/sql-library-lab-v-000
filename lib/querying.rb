4feb2a17a9ee50bed5ce84d49ee6f534abcb22f0
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 20:35:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
-  "
+  LEFT OUTER JOIN characters
+  ON series.id = characters.series_id
+ "
 end
 
 def select_character_names_and_number_of_books_they_are_in
